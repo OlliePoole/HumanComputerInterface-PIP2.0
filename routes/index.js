9ab68db82a40e8265f508d746a1762727ccdc2f0
oliver.t.poole <oliverpoole7@gmail.com>
2016-04-20 21:41:05 +0100

    Added files for seperate sidebars. Apologies to Alicia who is greatly offended by this

--- routes/index.js
+++ routes/index.js
@@ -8,53 +8,45 @@ exports.login = function(req, res){
 
 /* GET My PIP page */
 exports.myPIP = function(req, res) {
-  res.render('my-pip', { title: 'My PIP' });
+  res.render('my-pip', { title: 'My PIP', user: req.query.user, pageNum: 1 });
 };
 
 /* GET My Timetbale page */
 exports.timetable = function(req, res){
 
   // Semester One
-  var semesterOneMorningModules = [{name: "Machine Vision", time: "9AM", location: "T1.23"},
-                          {},
-                          {},
-                          {},
-                          {name: "Human Computer Interface", time: "9AM", location: "H1.23"}];
+  var semesterOneMorningModules = [{name: "Machine Vision", time: "9AM", location: "T1.23"}, {}, {}, {},
+                                   {name: "Human Computer Interface", time: "9AM", location: "H1.23"}];
 
-  var semesterOneAfternoonModules = [{},
-                          {},
-                          {name: "Web Technology", time: "1PM", location: "C1.13"},
-                          {},
-                          {}];
+  var semesterOneAfternoonModules = [{}, {},
+                                     {name: "Web Technology", time: "1PM", location: "C1.13"},
+                                     {}, {}];
 
   // Semester Two
-  var semesterTwoMorningModules = [{},
-                          {},
-                          {},
-                          {name: "Graph Theory", time: "9AM", location: "EL.17"},
-                          {}];
+  var semesterTwoMorningModules = [{}, {}, {},
+                                   {name: "Graph Theory", time: "9AM", location: "EL.17"},
+                                   {}];
 
   var semesterTwoAfternoonModules = [{},
-                          {name: "Advanced OOP", time: "1PM", location: "C2.12"},
-                          {name: "Data Structures", time: "1PM", location: "C1.13"},
-                          {},
-                          {}];
+                                     {name: "Advanced OOP", time: "1PM", location: "C2.12"},
+                                     {name: "Data Structures", time: "1PM", location: "C1.13"},
+                                     {}, {}];
 
   res.render('timetable-manager', { title: 'My Timetable',
-                                    semesterOne: {morning: semesterOneMorningModules,
-                                                  afternoon: semesterOneAfternoonModules},
-                                    semesterTwo: {morning: semesterTwoMorningModules,
-                                                  afternoon: semesterTwoAfternoonModules}
+                                    semesterOne: { morning: semesterOneMorningModules,
+                                                   afternoon: semesterOneAfternoonModules },
+                                    semesterTwo: { morning: semesterTwoMorningModules,
+                                                   afternoon: semesterTwoAfternoonModules },
+                                    pageNum: 2
                                     });
 };
 
 /* GET My Modules page */
 exports.modules = function(req, res) {
-
-  res.render('module-manager', { title: 'My Modules'});
+  res.render('module-manager', { title: 'My Modules', pageNum: 3});
 };
 
 /* GET My Settings page */
 exports.settings = function(req, res){
-  res.render('settings-manager', { title: 'My Settings' });
+  res.render('settings-manager', { title: 'My Settings', pageNum: 4});
 };
